ef4a6f2b16e3f555f43481d86f338042c5c7f643
Jordano Cubeddu <cubeddu05@gmail.com>
2020-10-01 19:38:12 +0200

    calculatrice finie avec keyCode

--- js/modules/fonctionement.js
+++ js/modules/fonctionement.js
@@ -1,4 +1,6 @@
 // recuperation de mes variables //
+let bodyCalculette = document.getElementsByTagName('body')[0];
+console.log(bodyCalculette);
 let inputCalculette = document.querySelector('input');
 let buttonCalculette = document.querySelectorAll('button');
 let buttonEgal = document.getElementsByClassName('egal')[0];
@@ -67,3 +69,51 @@ buttonEgal.addEventListener('click', () => {
         inputCalculette.value = parseInt(stock1) / parseInt(stock2)
     }
 })
+
+
+// creation des keyCode //
+bodyCalculette.addEventListener('keydown', (e) => {
+    let x = Number(e.key)
+    if (!isNaN(x)) {
+        inputCalculette.value += e.key
+    } else if (e.key == 'Backspace') {
+        inputCalculette.value = ''
+    } else if (e.key == 'c'){
+        inputCalculette.value = ''
+    } else if (e.key == '+') {
+        inputCalculette.value += '+'
+    } else if (e.key == "-") {
+        inputCalculette.value += '-'
+    } else if (e.key == '*') {
+        inputCalculette.value += '*'
+    } else if (e.key == '/') {
+        inputCalculette.value += '/'
+    } else if (e.key == '=') {
+        if (inputCalculette.value.includes('+')) {
+            let indexPlus = inputCalculette.value.indexOf('+');
+            stock1 = inputCalculette.value;
+            stock2 = inputCalculette.value.slice(indexPlus + 1, inputCalculette.value.length);
+            inputCalculette.value += buttonEgal.innerText;
+            inputCalculette.value = parseInt(stock1) + parseInt(stock2)
+        } else if (inputCalculette.value.includes('-')) {
+            let indexMoins = inputCalculette.value.indexOf('-');
+            stock1 = inputCalculette.value;
+            stock2 = inputCalculette.value.slice(indexMoins + 1, inputCalculette.value.length);
+            inputCalculette.value += buttonEgal.innerText;
+            inputCalculette.value = parseInt(stock1) - parseInt(stock2)
+        } else if (inputCalculette.value.includes('*')) {
+            let indexMulti = inputCalculette.value.indexOf('*');
+            stock1 = inputCalculette.value;
+            stock2 = inputCalculette.value.slice(indexMulti + 1, inputCalculette.value.length);
+            inputCalculette.value += buttonEgal.innerText;
+            inputCalculette.value = parseInt(stock1) * parseInt(stock2)
+        } else if (inputCalculette.value.includes('/')) {
+            let indexDivi = inputCalculette.value.indexOf('/');
+            stock1 = inputCalculette.value;
+            stock2 = inputCalculette.value.slice(indexDivi + 1, inputCalculette.value.length);
+            inputCalculette.value += buttonEgal.innerText;
+            inputCalculette.value = parseInt(stock1) / parseInt(stock2)
+        }
+    }
+})
+
